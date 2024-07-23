3af5e5fb36b417d8bc101b64bebc28ee222b4964
kibet nathan <nathanrufus540@gmail.com>
2024-07-23 10:44:04 +0300

    initial commit

---
+++ BookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookingSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
